1d73b7d0a6d990a135cbd91decaeb78a4dbbe42b
Amanda Truong <amandamtruong@gmail.com>
2025-06-04 14:07:38 -0500

    add robots meta tag

--- src/components/head.js
+++ src/components/head.js
@@ -59,6 +59,8 @@ const Head = ({ title, description, image }) => {
       <meta name="twitter:description" content={seo.description} />
       <meta name="twitter:image" content={seo.image} />
 
+      <meta name="robots" content="noindex, nofollow" /> {/* Remove this to allow google search results*/}
+
       <meta name="google-site-verification" content="DCl7VAf9tcz6eD9gb67NfkNnJ1PKRNcg8qQiwpbx9Lk" />
     </Helmet>
   );
